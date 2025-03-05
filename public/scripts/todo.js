7b96eae80fb17bb39bec0162574f62c5b60a5dc6
ml-begzad <elham1378basir@gmail.com>
2025-03-05 10:59:46 +0430

    first commit

---
+++ public/scripts/todo.js
@@ -0,0 +1,59 @@
+let listitem = JSON.parse(localStorage.getItem("arr")) || [
+  { task: "ES6", completed: false },
+  { task: "react", completed: false },
+  { task: "next", completed: false },
+];
+generateHTML();
+function add() {
+  let txt = document.querySelector("#inputField");
+  let todoname = txt.value;
+  if (todoname === "") {
+    alert("please enter something");
+  } else {
+    listitem.push({
+      task: todoname,
+      completed: false,
+    });
+    generateHTML();
+
+    txt.value = "";
+  }
+}
+function generateHTML() {
+  let acumelatorPattern = "";
+  for (let i = 0; i < listitem.length; i++) {
+    let list = listitem[i];
+
+    let completedClass = list.completed ? "completed" : "";
+    let html = `<li class="list-li ${completedClass}"><span>${list.task}</span><button class="li-delete">Delete</button></li>`;
+
+    acumelatorPattern += html;
+  }
+  document.querySelector("#list-ul").innerHTML = acumelatorPattern;
+
+  let deleteitems = document
+    .querySelectorAll(".li-delete")
+    .forEach((delbtn, index) => {
+      delbtn.addEventListener("click", () => {
+        listitem.splice(index, 1);
+        generateHTML();
+      });
+    });
+
+  let listLi = document
+    .querySelectorAll(".list-li")
+    .forEach((linethrough, index) => {
+      linethrough.addEventListener("click", () => {
+        if (listitem[index]) {
+          listitem[index].completed = !listitem[index].completed;
+
+          localStorage.setItem("arr", JSON.stringify(listitem));
+
+          linethrough.classList.toggle("completed");
+        }
+      });
+    });
+}
+let addbtn = document.querySelector("#btn").addEventListener("click", add);
+
+generateHTML();
